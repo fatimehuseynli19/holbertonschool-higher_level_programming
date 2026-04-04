803aaa218502e4695cdf9e5e2534e1d83db64d40
fatimehuseynli19 <13703@holbertonstudents.com>
2026-04-04 20:52:23 +0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[44:73] + str[82:87] + str[0:7])
+print(str[43:72] + str[82:87] + str[0:6])
 str = str
 # end
