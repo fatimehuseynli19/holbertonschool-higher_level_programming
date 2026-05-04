6a0680d3afc1dbf2869a03a1b775be0ae7f4c61e
fatimehuseynli19 <fatimehuseynli19@gmail.com>
2026-05-04 15:42:20 +0000

    Add docstrings to 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module for reading a text file and printing it to stdout."""
+
+
 def read_file(filename=""):
+    """Read a text file and print its content to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
