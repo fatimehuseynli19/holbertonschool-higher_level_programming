bfd6f2627353396f82f72145f03f809d65509878
fatimehuseynli19 <fatimehuseynli19@gmail.com>
2026-05-04 15:56:27 +0000

    Add 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with serialization support."""
+
+
+class Student:
+    """Defines a student by first_name, last_name and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance from a dict."""
+        for k, v in json.items():
+            setattr(self, k, v)
