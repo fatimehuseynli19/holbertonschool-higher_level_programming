a7f24ab3891d3a50d0cdedba5feb1f3e0b017181
fatimehuseynli19 <fatimehuseynli19@gmail.com>
2026-05-13 20:48:58 +0000

    fix case sensitive 2-my_filter_states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,9 +14,8 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-            sys.argv[4]
-        )
+        "SELECT * FROM states WHERE BINARY name = '{}'"
+        " ORDER BY id ASC".format(sys.argv[4])
     )
     for row in cursor.fetchall():
         print(row)
