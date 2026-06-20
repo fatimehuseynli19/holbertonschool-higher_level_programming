eb7b33612e0fdfc2c6a4251c194ef9026cf3d55c
fatimehuseynli19 <13703@holbertonstudents.com>
2026-06-20 18:02:40 +0200

    8. ByteCode -> Python #3

---
+++ python-import_modules/magic_calculation_102.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Helper module with add and sub functions"""
+
+
+def add(a, b):
+    """Add two numbers"""
+    return a + b
+
+
+def sub(a, b):
+    """Subtract two numbers"""
+    return a - b
